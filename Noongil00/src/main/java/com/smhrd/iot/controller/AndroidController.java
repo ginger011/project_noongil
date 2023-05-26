c82919bf60e28bc67ae99e5979116047c839b4cf
heekyung <gonghk0607@gmail.com>
2023-05-26 16:45:59 +0900

    android controller수정

--- Noongil00/src/main/java/com/smhrd/iot/controller/AndroidController.java
+++ Noongil00/src/main/java/com/smhrd/iot/controller/AndroidController.java
@@ -1,8 +1,40 @@
 package com.smhrd.iot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.smhrd.iot.domain.UserInfo;
+import com.smhrd.iot.service.UserService;
+
+
+@RestController
 public class AndroidController {
+	// 사용자 로그인
+	//@PostMapping(value="")
+	//public String userlogin(String userID, String userPW, HttpSession session) {
+//		System.out.println("id: " + userID + "pw: " + userPW);
+//		int result = service.userLogin(userID, userPW);
+//				
+//		if(result == 1) { // 로그인 성공
+//			System.out.println("성공: " + result);
+//			// 세션에 ID 저장
+//			session.setAttribute("userID", userID);
+//			return "userReport";
+//		}else { // 로그인 실패
+//			System.out.println("실패: " + result);
+//			return "index";
+//		}
+	//}
 
+	@Autowired
+	private UserService service;
+	
+	@PostMapping(value="/androidApp")
+	 public void createAppMember() {
+     System.out.println("받아짐");
+  }
+	
 }
