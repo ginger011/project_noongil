12ffd651a053fcf8d711d86134b9634fe2770da8
Banana-Master <tjdgh0313@gmail.com>
2023-05-26 11:32:09 +0900

    주소를 localhost:8085/noongil로 변경했음

--- Noongil00/src/main/java/com/smhrd/iot/controller/HomeController.java
+++ Noongil00/src/main/java/com/smhrd/iot/controller/HomeController.java
@@ -16,51 +16,23 @@ public class HomeController {
 	
 	@GetMapping(value="/")
 	public String basic() {
-		return "basic";
+		return "index";
 	}
 	// test
 	//test2
 	//test3
 	
-	@GetMapping(value="/thym")
-	public String thym(Model model, HttpSession session) {
-		//Model:request객체랑 비슷한 역할 (데이터 저장)
-		//텍스트 "테스트"
-		//주석
-		// testt2
-		model.addAttribute("test","테스트");
-		
-		Board b = new Board();
-		b.setIdx(1);
-		b.setTitle("제목");
-		b.setContent("내용");
-		b.setWriter("작성자");
-		b.setIndate("2023-05-02");
-		
-		Board b2 = new Board();
-		b2.setIdx(2);
-		b2.setTitle("제목2");
-		b2.setContent("내용2");
-		b2.setWriter("작성자2");
-		b2.setIndate("2023-05-02");
-		
-		Board b3 = new Board();
-		b3.setIdx(3);
-		b3.setTitle("제목3");
-		b3.setContent("내용3");
-		b3.setWriter("작성자3");
-		b3.setIndate("2023-05-02");
-		
-		List<Board> list = new ArrayList<Board>();
-		list.add(b);
-		list.add(b2);
-		list.add(b3);
-		
-		model.addAttribute("board", b);
-		model.addAttribute("list", list);
-		
-		session.setAttribute("board2", b2);
-		
-		return "thymeleaf";
+	@GetMapping(value="/map")
+	public String map() {
+		return "map";
+	}
+	
+	@GetMapping(value="/user")
+	public String user() {
+		return "user";
+	}
+	@GetMapping(value="/userReport")
+	public String userReport() {
+		return "userReport";
 	}
 }
