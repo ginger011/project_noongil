808a8051e84fb5386e77a9b9639bc547b38341c5
Totoclub12 <stj4096@naver.com>
2023-05-30 10:07:36 +0900

    태정 풀용 1

--- Noongil00/src/main/resources/static/js/report.js
+++ Noongil00/src/main/resources/static/js/report.js
@@ -6,7 +6,7 @@
 			type : "get", //요청방식(get/post)
 			dataType : "json",   //서버 반환 데이터 타입
 			success : function(data){
-				console.log(data)
+				console.log("난 머지"+data)
 			},  //리스트 출력해주는 함수 호출
 			error : function(){
 				alert("통신실패!")
