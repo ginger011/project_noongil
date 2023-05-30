16cfb4c05dabfc446a9c66238016771c49b23470
heekyung <gonghk0607@gmail.com>
2023-05-30 12:40:15 +0900

    Merge branch 'master0' of https://github.com/2021-SMHRD-KDT-IoT-9/Nungil.git into master0
    안드로이드 로그인 기능 추가

--- Noongil00/src/main/java/com/smhrd/iot/domain/Report.java
+++ Noongil00/src/main/java/com/smhrd/iot/domain/Report.java
@@ -14,7 +14,7 @@ public class Report {
 	private int userResportNum;
 	private String userID;
 	private String userReportPlace;
-	private String usetReportContent;
+	private String userReportContent;
 	private String userReportState;
 	private String userReportDate;
 
--- Noongil00/src/main/java/com/smhrd/iot/service/AndroidService.java
+++ Noongil00/src/main/java/com/smhrd/iot/service/AndroidService.java
@@ -3,18 +3,29 @@ package com.smhrd.iot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.smhrd.iot.domain.UserAndroidInfo;
 import com.smhrd.iot.domain.UserInfo;
-//import com.smhrd.iot.mapper.AndroidMapper;
+import com.smhrd.iot.mapper.AndroidMapper;
 
 @Service
 public class AndroidService {
 	
-//	@Autowired
-//	private AndroidMapper mapper;
+@Autowired
+private AndroidMapper mapper;
 	
-//	public UserInfo joinAndroid(UserInfo uInfo) {
-//		mapper.userAndroidJoin(uInfo);
-//		return null;
-//	}
+public UserInfo userAndroidJoin(UserInfo uInfo) {
+	System.out.println(uInfo.getCaneId());
+	mapper.userAndroidJoin(uInfo);
+	return uInfo;
+}
+
+public UserAndroidInfo userAndroidLogin(UserAndroidInfo userInfoLogin) {
+    int result = mapper.userAndroidLogin(userInfoLogin);
+    if (result == 1) { // 로그인 성공
+        return userInfoLogin;
+    } else { // 로그인 실패
+        return null;
+    }
+}
 	
 }
