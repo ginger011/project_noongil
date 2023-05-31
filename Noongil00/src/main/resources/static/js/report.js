0fe086d54c53c48b939a757e48b238ab7d359017
Banana-Master <tjdgh0313@gmail.com>
2023-05-31 12:44:55 +0900

    report 중간저

--- Noongil00/src/main/resources/static/js/report.js
+++ Noongil00/src/main/resources/static/js/report.js
@@ -10,7 +10,6 @@ function loadbreport() {
 			alert("통신실패!");
 		}
 	});
-
 	$.when(
 		$.ajax({
 			url: "api/breport/",
@@ -31,14 +30,11 @@ function loadbreport() {
 			alert("통신실패!");
 		});
 }
-
-  
-
 function populateTable1(data) {
 	let result = '';
-	for(let i = 0; i < data.length; i++) {
-		console.log(data[i])
-		result += "<tr>"
+	for (let i = 0; i < data.length; i++) {
+		console.log(data[i]);
+		result += "<tr>";
 		// 번호
 		result += "<th><span>" + data[i]['userReportNum'] + "</span></th>";
 		// 구분
@@ -50,35 +46,53 @@ function populateTable1(data) {
 		// 내용
 		result += "<th><span>" + data[i]['userReportContent'] + "</span></th>";
 		// 상태
-		result += "<th><span>" + data[i]['userReportState'] + "</span></th>";
+		result += "<th><span class='state' onclick='showAlert(this)'>" + data[i]['userReportState'] + "</span></th>";
 		result += "</tr>";
 	}
 	document.querySelector('.user-div tbody').innerHTML += result;
-
 }
 
 
 function populateTable2(data) {
-  
-		let result = '';
-		for(let i = 0; i < data.length; i++) {
-			console.log(data[i])
-		  result += "<tr>"
-		  // 번호
-		  result += "<th><span>" + data[i]['autoReportNum'] + "</span></th>";
-		  // 구분
-		  result += "<th><span>" + data[i]['autoID'] + "</span></th>";
-		  // 작성일자
-		  result += "<th><span>" + data[i]['autoReportDate'] + "</span></th>";
-		  // 위치
-		  result += "<th><span>" + data[i]['autoReportPlace'] + "</span></th>";
-		  // 내용
-		  result += "<th><span>" + data[i]['autoReportContent'] + "</span></th>";
-		  // 상태
-		  result += "<th><span>" + data[i]['autoReportState'] + "</span></th>";
-		  result += "</tr>"
-		}
-		document.querySelector('.user-div tbody').innerHTML += result;
+	let result = '';
+	for (let i = 0; i < data.length; i++) {
+		console.log(data[i]);
+		result += "<tr>";
+		// 번호
+		result += "<th><span>" + data[i]['autoReportNum'] + "</span></th>";
+		// 구분
+		result += "<th><span>" + data[i]['autoID'] + "</span></th>";
+		// 작성일자
+		result += "<th><span>" + data[i]['autoReportDate'] + "</span></th>";
+		// 위치
+		result += "<th><span>" + data[i]['autoReportPlace'] + "</span></th>";
+		// 내용
+		result += "<th><span>" + data[i]['autoReportContent'] + "</span></th>";
+		// 상태
+		result += "<th><span class='state' onclick='showAlert(this)'>" + data[i]['autoReportState'] + "</span></th>";
+		result += "</tr>";
+	}
+	document.querySelector('.user-div tbody').innerHTML += result;
+}
 
-  
+function showAlert(element) {
+    console.log(element.textContent);
+
+    const existingBox = document.querySelector('.status-box');
+    // existingBox가 존재하면, 즉 .status-box 요소가 이미 있으면 다음 코드를 실행합니다.
+	console.log(existingBox)
+    if (existingBox) {
+        existingBox.style.display = "block"; // 기존 박스를 보이도록 스타일을 변경합니다.
+        console.log("hihi"); // "hihi"를 콘솔에 출력합니다.
+    }
+
+    const statusBtns = document.querySelector(".status-btn button");
+
+    // statusBtns에 대해 forEach 루프를 실행합니다.
+    statusBtns.forEach(btn => {
+        // 각 버튼에 클릭 이벤트 리스너를 추가합니다.
+        btn.addEventListener("click", () => {
+            existingBox.style.display = "none"; // 박스를 숨기도록 스타일을 변경합니다.
+        });
+    });
 }
