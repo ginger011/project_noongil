7a2bfcc1b77ba657ea4a6d349aeb6256ac6946bf
Banana-Master <tjdgh0313@gmail.com>
2023-05-31 10:35:34 +0900

    회원정보 수정하기틀 완성

--- Noongil00/src/main/resources/static/js/report.js
+++ Noongil00/src/main/resources/static/js/report.js
@@ -2,34 +2,34 @@
 
 function loadbreport() {
 	$.ajax({
-	  url: "api/breport/",  //요청경로
-	  type: "get", //요청방식(get/post)
-	  dataType: "json",   //서버 반환 데이터 타입
-	  success: function (data) {
-		let result = '';
-		for(let i = 0; i < data.length; i++) {
-			console.log(data[i])
-		  result += "<tr>"
-		  // 번호
-		  result += "<th><span>" + data[i]['userReportNum'] + "</span></th>";
-		  // 구분
-		  result += "<th><span>" + data[i]['userID'] + "</span></th>";
-		  // 작성일자
-		  result += "<th><span>" + data[i]['userReportDate'] + "</span></th>";
-		  // 위치
-		  result += "<th><span>" + data[i]['userReportPlace'] + "</span></th>";
-		  // 내용
-		  result += "<th><span>" + data[i]['userReportContent'] + "</span></th>";
-		  // 상태
-		  result += "<th><span>" + data[i]['userReportState'] + "</span></th>";
-		  result += "</tr>"
+		url: "api/breport/",  //요청경로
+		type: "get", //요청방식(get/post)
+		dataType: "json",   //서버 반환 데이터 타입
+		success: function (data) {
+			let result = '';
+			for (let i = 0; i < data.length; i++) {
+				console.log(data[i])
+				result += "<tr>"
+				// 번호
+				result += "<th><span>" + data[i]['userReportNum'] + "</span></th>";
+				// 구분
+				result += "<th><span>" + data[i]['userID'] + "</span></th>";
+				// 작성일자
+				result += "<th><span>" + data[i]['userReportDate'] + "</span></th>";
+				// 위치
+				result += "<th><span>" + data[i]['userReportPlace'] + "</span></th>";
+				// 내용
+				result += "<th><span>" + data[i]['userReportContent'] + "</span></th>";
+				// 상태
+				result += "<th><span>" + data[i]['userReportState'] + "</span></th>";
+				result += "</tr>"
+			}
+			document.querySelector('.user-div tbody').innerHTML = result;
+		},
+		error: function () {
+			alert("통신실패!")
 		}
-		document.querySelector('.user-div tbody').innerHTML = result;
-	  },
-	  error: function () {
-		alert("통신실패!")
-	  }
 	})
-  }
-  
+}
+
 
