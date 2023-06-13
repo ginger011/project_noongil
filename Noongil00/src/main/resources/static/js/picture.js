dd52f45e85abb873ccd0d49ff1f6ee064b25dba7
Banana-Master <tjdgh0313@gmail.com>
2023-06-13 18:21:55 +0900

    Merge branch 'master0' of https://github.com/2021-SMHRD-KDT-IoT-9/Nungil.git into master0

--- Noongil00/src/main/resources/static/js/picture.js
+++ Noongil00/src/main/resources/static/js/picture.js
@@ -1,8 +1,8 @@
 const slidePicture = document.getElementById('threebox-1').getElementsByTagName('img');
 
 const imgURL = [
-    'https://cdn.imweb.me/upload/S202207202685e30f16e24/d0812bfa4755e.jpg',
-    'https://cdn.imweb.me/upload/S202207202685e30f16e24/8b48c67f8cdf6.jpeg'
+    './img/점자블록사진.png',
+    './img/지팡이사진.png'
 ];
 // 첫번째는 html의 src로 한번 보여주기때문에 2번째부터 시작한다.
 let currentIndex = 1;
